0077fcda16f5b3400bc17dd8f0bb828338c30afc
ManoolK <koshaksu@gmail.com>
2021-05-06 13:29:49 -0400

    fundamentals

---
+++ fundamentals/assignments.js
@@ -0,0 +1,14 @@
+const country = "Russia";
+const continent = "Eurasia";
+let population = "144.4";
+const isIsland = false;
+let language;
+
+console.log(country + " in " + continent + " has population of " + population + " millions.");
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = 'russian';
---
+++ fundamentals/script.js
@@ -0,0 +1,25 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+
+let jsIsFun = true;
+console.log(typeof jsIsFun);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Smith';
+console.log(lastName);
+*/
+
