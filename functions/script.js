e69b80068b5609c25e8e13f2aa9053c05f30ef28
ManoolK <koshaksu@gmail.com>
2021-06-22 15:23:51 -0400

    Functions. Challenge #1

--- functions/script.js
+++ functions/script.js
@@ -65,8 +65,71 @@ const greet = function (greeting) {
   };
 };
 
-greet('Hey')('Oxana');
+// greet('Hey')('Oxana');
 
 const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
 
-greetArrow('Hello')('Kate');
+// greetArrow('Hello')('Kate');
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+    this.bookings.push({flight: `${this.iataCode}${flightNum}`, name});
+  },
+};
+
+const book = lufthansa.book;
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+// lufthansa.book(239, 'Oxana ManoolK');
+// lufthansa.book(635, 'John Doe');
+
+// book.call(eurowings, 23, 'Srah Williams');
+// console.log(eurowings);
+
+// Apply method (rarely used nowadays)
+const flightData = [583, 'George Cooper'];
+// book.apply(eurowings, flightData);
+
+// book.call(eurowings, ...flightData);
+
+// bind method - for manualy set the this word. Returns a new function.
+const bookEW = book.bind(eurowings);
+const bookEW23 = book.bind(eurowings, 23); // preset flightNum
+
+// bookEW(23, 'Steven Williams');
+// bookEW23('Martha Williams');
+
+// With event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function() {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+}
+
+// problem with this with event listener
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// console.log(addVAT(100));
+
+const addTax2 = function(rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTax2(0.2);
+// console.log(addVAT2(100));
